d518bb6cc3adbe180b715359b9e452804774d0a4
Marco Castañeda <marco_pascual410@hotmail.com>
2026-02-05 15:57:14 -0600

    Update constans.ts

--- src/constants/constans.ts
+++ src/constants/constans.ts
@@ -2,5 +2,5 @@ export const SOCIAL_LINKS = [
     { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61576878181992', icon: '/icons/fb.svg' },
   ];
 
-  export const DEFAULT_ANIMALS_PER_PAGE = 12;
-export const DEFAULT_RADIUS = 1000;
+export const DEFAULT_ANIMALS_PER_PAGE = 12;
+export const DEFAULT_RADIUS = 50;
